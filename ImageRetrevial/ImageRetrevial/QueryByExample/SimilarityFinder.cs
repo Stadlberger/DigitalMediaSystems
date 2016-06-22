c7ba6a83eecb2c94028b48a15b806db881315e04
Philipp Wimmer <pwimmer.mmt-b2014@fh-salzburg.ac.at>
2016-06-22 16:25:50 +0200

    SimilarityFinder returns ISearchResults

--- ImageRetrevial/ImageRetrevial/QueryByExample/SimilarityFinder.cs
+++ ImageRetrevial/ImageRetrevial/QueryByExample/SimilarityFinder.cs
@@ -9,16 +9,19 @@ using System.Threading.Tasks;
 using Accord.Imaging;
 using Accord.MachineLearning;
 using Accord.Math;
+using ImageRetrevial;
 
 namespace QueryImage
 {
     class SimilarityFinder
     {
         private Dictionary<string, double[]> savedImages;
+        private DataController dataController;
 
 
-        public SimilarityFinder(string assetPath, string features)
+        public SimilarityFinder(string assetPath, string features, DataController dataController)
         {
+            this.dataController = dataController;
             savedImages = new Dictionary<string, double[]>();
             foreach (var url in Directory.GetFiles(assetPath))
             {
@@ -55,7 +58,7 @@ namespace QueryImage
             }
         }
 
-        public List<Tuple<string,double>> getSimilarImages(string index, int count)
+        public List<Tuple<string,double>> getSimilarImagesVerbose(string index, int count)
         {
             DateTime start = DateTime.Now;
             List <Tuple<string,double>> images = new List<Tuple<string, double>>();
@@ -74,34 +77,71 @@ namespace QueryImage
             return images.GetRange(0, count);
         }
 
-        public List<Tuple<string, double>> getSimilarImagesByFile(string path, int count)
+        public List<ISearchResult> getSimilarImages(string index, int count)
         {
-            Bitmap refImg = (Bitmap)Bitmap.FromFile(path);
-            HistogramsOfOrientedGradients hog = new HistogramsOfOrientedGradients();
-
-            List<double> tempDoubles = new List<double>();
-
-            foreach (var darray in hog.ProcessImage(refImg))
-            {
-                foreach (var doub in darray)
-                {
-                    tempDoubles.Add(doub);
-                }
-            }
-
-            double[] currImage = tempDoubles.ToArray();
-
-
-            DateTime start = DateTime.Now;
             List<Tuple<string, double>> images = new List<Tuple<string, double>>();
             foreach (var kvp in savedImages)
             {
-                double dist = currImage.Euclidean(kvp.Value);
+                double dist = savedImages[index].Euclidean(kvp.Value);
                 images.Add(new Tuple<string, double>(kvp.Key, dist));
             }
+            /*Parallel.ForEach(savedImages, kvp =>
+            {
+                double dist = savedImages[index].Euclidean(kvp.Value);
+                lock (images) images.Add(new Tuple<string, double>(kvp.Key, dist));
+            });*/
             images.Sort((tuple, tuple1) => tuple.Item2 > tuple1.Item2 ? 1 : -1);
-            Console.Write("This took " + DateTime.Now.Subtract(start).TotalMilliseconds + " milliseconds");
-            return images.GetRange(0, count);
+
+            List<ISearchResult> SearchResults = new List<ISearchResult>();
+
+            QueryData[] qData = new QueryData[1];
+
+            for (int i = 0; i < images.Count; i++)
+            {
+                QueryData data = new QueryData();
+                data.m_fieldName = "id";
+                data.m_fieldValue = images[i].Item1;
+
+                qData[0] = data;
+
+                SearchResults.Add(dataController.RunQuery(qData).ToList()[0]);
+            }
+            //Perform Search
+            //Get Results ... return List<ISearchResult>
+            //Display them
+            
+
+            return SearchResults;
         }
+
+        //public List<Tuple<string, double>> getSimilarImagesByFile(string path, int count)
+        //{
+        //    Bitmap refImg = (Bitmap)Bitmap.FromFile(path);
+        //    HistogramsOfOrientedGradients hog = new HistogramsOfOrientedGradients();
+
+        //    List<double> tempDoubles = new List<double>();
+
+        //    foreach (var darray in hog.ProcessImage(refImg))
+        //    {
+        //        foreach (var doub in darray)
+        //        {
+        //            tempDoubles.Add(doub);
+        //        }
+        //    }
+
+        //    double[] currImage = tempDoubles.ToArray();
+
+
+        //    DateTime start = DateTime.Now;
+        //    List<Tuple<string, double>> images = new List<Tuple<string, double>>();
+        //    foreach (var kvp in savedImages)
+        //    {
+        //        double dist = currImage.Euclidean(kvp.Value);
+        //        images.Add(new Tuple<string, double>(kvp.Key, dist));
+        //    }
+        //    images.Sort((tuple, tuple1) => tuple.Item2 > tuple1.Item2 ? 1 : -1);
+        //    Console.Write("This took " + DateTime.Now.Subtract(start).TotalMilliseconds + " milliseconds");
+        //    return images.GetRange(0, count);
+        //}
     }
 }
